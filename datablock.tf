ee97c0b76575ead3fd89213cdb8ee9a36ace2f24
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2025-05-11 23:00:09 +0530

    Create datablock.tf

---
+++ datablock.tf
@@ -0,0 +1,22 @@
+# Data source for VPC (default VPC)
+data "aws_vpc" "selected" {
+  default = true
+}
+
+
+
+# Data source for AMI (latest Ubuntu image)
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
