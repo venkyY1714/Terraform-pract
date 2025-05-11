b45ff7c4bf7bef2936454b72def8bcef9b72955f
prudhvisurya996 <prudhvisurya996@gmail.com>
2025-05-11 23:50:41 +0530

    m5

--- modules/EC2/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-
-    bucket  = "statefilestorage8780"
-    key     = "mystatefolder2/terraform.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
-  }
-}
--- modules/security_group/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  profile = "default"
-  region  = "ap-south-1"
-}
