0cd9da2b2a52dee2bada35cb0118449013ee1192
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2025-05-13 17:02:41 +0530

    Create output.tf

---
+++ vpc/output.tf
@@ -0,0 +1,34 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.My-vpc.id
+}
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  description = "The ID of the private subnet"
+  value       = aws_subnet.private_subnet.id
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.My-igw.id
+}
+
+output "nat_gateway_id" {
+  description = "The ID of the NAT Gateway"
+  value       = aws_nat_gateway.nat_gw.id
+}
+
+output "public_route_table_id" {
+  description = "The ID of the public route table"
+  value       = aws_route_table.public_rt.id
+}
+
+output "private_route_table_id" {
+  description = "The ID of the private route table"
+  value       = aws_route_table.private_rt.id
+}
