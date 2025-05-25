6c95fc04aa4e623a804581a27e90a32cae07c245
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2025-05-25 18:41:08 +0530

    Update output.tf

--- EC2/EC2-latest/output.tf
+++ EC2/EC2-latest/output.tf
@@ -12,3 +12,11 @@ output "instance_private_ip" {
   description = "The private IP address of the EC2 instance"
   value       = aws_instance.myinstance.private_ip
 }
+
+output "key_name" {
+  value = aws_key_pair.terraform_key.key_name
+}
+
+output "private_key_path" {
+  value = local_file.private_key_pem.filename
+}
