c11591c342bbf7113733c1576a8f06c15a737fc3
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2025-05-25 17:48:40 +0530

    Update main.tf

---
+++ EC2/pemfile/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# 1. Generate an RSA key pair
+resource "tls_private_key" "terraform_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# 2. Upload the public key to AWS as a key pair
+resource "aws_key_pair" "terraform_key" {
+  key_name   = "terraform-generated-key"
+  public_key = tls_private_key.terraform_key.public_key_openssh
+}
+
+# 3. Save the private key locally as a .pem file
+resource "local_file" "private_key_pem" {
+  content         = tls_private_key.terraform_key.private_key_pem
+  filename        = "${path.module}/terraform-key.pem"
+  file_permission = "0600"
+}
+
