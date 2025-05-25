9441d33037a4105c70b17d0de7ba866382c8e98e
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2025-05-25 18:30:11 +0530

    Update resource.tf

--- EC2/EC2-latest/resource.tf
+++ EC2/EC2-latest/resource.tf
@@ -1,30 +1,29 @@
-resource "tls_private_key" "example" {
+# Generate SSH Private Key
+resource "tls_private_key" "terraform_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+# Upload Public Key to AWS as a Key Pair
 resource "aws_key_pair" "generated_key" {
   key_name   = "terraform-generated-key"
-  public_key = tls_private_key.example.public_key_openssh
+  public_key = tls_private_key.terraform_key.public_key_openssh
 }
 
-resource "aws_instance" "myinstance" {
-  ami           =  data.aws_ami.ubuntu.id
-  instance_type = var.myinstype
-  key_name      = aws_key_pair.generated_key.key_name            
-  tags = {
-    Name = "My EC2 Instance"
-  }
-}
-
-
+# Save Private Key to Local File
 resource "local_file" "private_key_pem" {
   content         = tls_private_key.terraform_key.private_key_pem
   filename        = "${path.module}/terraform-key.pem"
   file_permission = "0600"
 }
 
+# Create EC2 Instance using the uploaded Key Pair
+resource "aws_instance" "myinstance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.myinstype
+  key_name      = aws_key_pair.generated_key.key_name
 
-
-
-
+  tags = {
+    Name = "My EC2 Instance"
+  }
+}
